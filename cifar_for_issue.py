5977de48227be9892a0f5af17d5a896d7c52d98c
ucLh <luchwork@mail.ru>
2022-06-08 11:27:31 +0300

    Add a shortened train script for the issue

--- cifar_for_issue.py
+++ cifar_for_issue.py
@@ -183,45 +183,6 @@ def train_model(model,
     return model
 
 
-def calibrate_model(model, loader, device=torch.device("cpu:0")):
-
-    model.to(device)
-    model.eval()
-
-    for inputs, labels in loader:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        _ = model(inputs)
-
-
-def measure_inference_latency(model,
-                              device,
-                              input_size=(1, 3, 32, 32),
-                              num_samples=100,
-                              num_warmups=10):
-
-    model.to(device)
-    model.eval()
-
-    x = torch.rand(size=input_size).to(device)
-
-    with torch.no_grad():
-        for _ in range(num_warmups):
-            _ = model(x)
-    torch.cuda.synchronize()
-
-    with torch.no_grad():
-        start_time = time.time()
-        for _ in range(num_samples):
-            _ = model(x)
-            torch.cuda.synchronize()
-        end_time = time.time()
-    elapsed_time = end_time - start_time
-    elapsed_time_ave = elapsed_time / num_samples
-
-    return elapsed_time_ave
-
-
 def save_model(model, model_dir, model_filename):
 
     if not os.path.exists(model_dir):
@@ -245,7 +206,6 @@ def save_torchscript_model(model, model_dir, model_filename):
     input_ = torch.rand(size=(1, 3, 32, 32)).to('cpu')
     traced_model = torch.jit.trace(model, input_)
     torch.jit.save(traced_model, model_filepath)
-    # torch.onnx.export(model, input_, './saved_models/resnet_quant.onnx', opset_version=11)
 
 
 def load_torchscript_model(model_filepath, device):
@@ -253,73 +213,6 @@ def load_torchscript_model(model_filepath, device):
     model = torch.jit.load(model_filepath, map_location=device)
 
     return model
-
-
-def create_model(num_classes=10):
-
-    # The number of channels in ResNet18 is divisible by 8.
-    # This is required for fast GEMM integer matrix multiplication.
-    # model = torchvision.models.quantization.resnet18(pretrained=False, num_classes=num_classes)
-    model = resnet18(num_classes=num_classes, pretrained=False)
-
-    # We would use the pretrained ResNet18 as a feature extractor.
-    # for param in model.parameters():
-    #     param.requires_grad = False
-
-    # Modify the last FC layer
-    # num_features = model.fc.in_features
-    # model.fc = nn.Linear(num_features, 10)
-
-    return model
-
-
-class QuantizedResNet18(nn.Module):
-    def __init__(self, model_fp32):
-
-        super(QuantizedResNet18, self).__init__()
-        # QuantStub converts tensors from floating point to quantized.
-        # This will only be used for inputs.
-        self.quant = torch.quantization.QuantStub()
-        # DeQuantStub converts tensors from quantized to floating point.
-        # This will only be used for outputs.
-        self.dequant = torch.quantization.DeQuantStub()
-        # FP32 model
-        self.model_fp32 = model_fp32
-
-    def forward(self, x):
-        # manually specify where tensors will be converted from floating
-        # point to quantized in the quantized model
-        x = self.quant(x)
-        x = self.model_fp32(x)
-        # manually specify where tensors will be converted from quantized
-        # to floating point in the quantized model
-        x = self.dequant(x)
-        return x
-
-
-def model_equivalence(model_1,
-                      model_2,
-                      device,
-                      rtol=1e-05,
-                      atol=1e-08,
-                      num_tests=100,
-                      input_size=(1, 3, 32, 32)):
-
-    model_1.to(device)
-    model_2.to(device)
-
-    for _ in range(num_tests):
-        x = torch.rand(size=input_size).to(device)
-        y1 = model_1(x).detach().cpu().numpy()
-        y2 = model_2(x).detach().cpu().numpy()
-        if np.allclose(a=y1, b=y2, rtol=rtol, atol=atol,
-                       equal_nan=False) == False:
-            print("Model equivalence test sample failed: ")
-            print(y1)
-            print(y2)
-            return False
-
-    return True
 
 
 def main():
@@ -331,89 +224,20 @@ def main():
     torch.backends.quantized.engine = 'qnnpack'
 
     model_dir = "saved_models"
-    model_filename = "resnet18_cifar10.pt"
     quantized_model_filename = "resnet18_quantized_cifar10.pt"
-    model_filepath = os.path.join(model_dir, model_filename)
     quantized_model_filepath = os.path.join(model_dir,
                                             quantized_model_filename)
 
     set_random_seeds(random_seed=random_seed)
 
-    # Create an untrained model.
-    model = create_model(num_classes=num_classes)
-
     train_loader, test_loader = prepare_dataloader(num_workers=8,
                                                    train_batch_size=128,
                                                    eval_batch_size=256)
 
-    # Train model.
-    print("Training Model...")
-    model = train_model(model=model,
-                        train_loader=train_loader,
-                        test_loader=test_loader,
-                        device=cuda_device,
-                        learning_rate=1e-2,
-                        num_epochs=100)
-    # Save model.
-    save_model(model=model, model_dir=model_dir, model_filename=model_filename)
-    # Load a pretrained model.
-    model = load_model(model=model,
-                       model_filepath=model_filepath,
-                       device=cuda_device)
-    # Move the model to CPU since static quantization does not support CUDA currently.
-    model.to(cpu_device)
-    # Make a copy of the model for layer fusion
-    fused_model = copy.deepcopy(model)
-
-    model.train()
-    # The model has to be switched to training mode before any layer fusion.
-    # Otherwise the quantization aware training will not work correctly.
-    fused_model.train()
-
-    # Fuse the model in place rather manually.
-    fused_model = torch.quantization.fuse_modules(fused_model,
-                                                  [["conv1", "bn1", "relu"]],
-                                                  inplace=True)
-    for module_name, module in fused_model.named_children():
-        if "layer" in module_name:
-            for basic_block_name, basic_block in module.named_children():
-                torch.quantization.fuse_modules(
-                    basic_block, [["conv1", "bn1", "relu1"], ["conv2", "bn2"]],
-                    inplace=True)
-                for sub_block_name, sub_block in basic_block.named_children():
-                    if sub_block_name == "downsample":
-                        torch.quantization.fuse_modules(sub_block,
-                                                        [["0", "1"]],
-                                                        inplace=True)
-                    if sub_block_name == "se":
-                        torch.quantization.fuse_modules(sub_block,
-                                                        [["linear1", "relu"]],
-                                                        inplace=True)
-
-    # Print FP32 model.
-    print(model)
-    # Print fused model.
-    print(fused_model)
-
-    # Model and fused model should be equivalent.
-    model.eval()
-    fused_model.eval()
-    assert model_equivalence(
-        model_1=model,
-        model_2=fused_model,
-        device=cpu_device,
-        rtol=1e-03,
-        atol=1e-06,
-        num_tests=100,
-        input_size=(
-            1, 3, 32,
-            32)), "Fused model is not equivalent to the original model!"
-
     # Prepare the model for quantization aware training. This inserts observers in
     # the model that will observe activation tensors during calibration.
-    quantized_model = QuantizedResNet18(model_fp32=fused_model)
-    # quantized_model = torchvision.models.quantization.resnet18(pretrained=False, num_classes=num_classes)
-    # quantized_model.fuse_model()
+    quantized_model = torchvision.models.quantization.resnet18(pretrained=False, num_classes=num_classes)
+    quantized_model.fuse_model()
     # Using un-fused model will fail.
     # Because there is no quantized layer implementation for a single batch normalization layer.
     # quantized_model = QuantizedResNet18(model_fp32=model)
@@ -465,50 +289,12 @@ def main():
     quantized_jit_model = load_torchscript_model(
         model_filepath=quantized_model_filepath, device=cpu_device)
 
-    _, fp32_eval_accuracy = evaluate_model(model=model,
-                                           test_loader=test_loader,
-                                           device=cpu_device,
-                                           criterion=None)
     _, int8_eval_accuracy = evaluate_model(model=quantized_jit_model,
                                            test_loader=test_loader,
                                            device=cpu_device,
                                            criterion=None)
 
-    # Skip this assertion since the values might deviate a lot.
-    # assert model_equivalence(model_1=model, model_2=quantized_jit_model, device=cpu_device, rtol=1e-01, atol=1e-02, num_tests=100, input_size=(1,3,32,32)), "Quantized model deviates from the original model too much!"
-
-    print("FP32 evaluation accuracy: {:.3f}".format(fp32_eval_accuracy))
     print("INT8 evaluation accuracy: {:.3f}".format(int8_eval_accuracy))
-
-    fp32_cpu_inference_latency = measure_inference_latency(model=model,
-                                                           device=cpu_device,
-                                                           input_size=(1, 3,
-                                                                       32, 32),
-                                                           num_samples=100)
-    int8_cpu_inference_latency = measure_inference_latency(
-        model=quantized_model,
-        device=cpu_device,
-        input_size=(1, 3, 32, 32),
-        num_samples=100)
-    int8_jit_cpu_inference_latency = measure_inference_latency(
-        model=quantized_jit_model,
-        device=cpu_device,
-        input_size=(1, 3, 32, 32),
-        num_samples=100)
-    fp32_gpu_inference_latency = measure_inference_latency(model=model,
-                                                           device=cuda_device,
-                                                           input_size=(1, 3,
-                                                                       32, 32),
-                                                           num_samples=100)
-
-    print("FP32 CPU Inference Latency: {:.2f} ms / sample".format(
-        fp32_cpu_inference_latency * 1000))
-    print("FP32 CUDA Inference Latency: {:.2f} ms / sample".format(
-        fp32_gpu_inference_latency * 1000))
-    print("INT8 CPU Inference Latency: {:.2f} ms / sample".format(
-        int8_cpu_inference_latency * 1000))
-    print("INT8 JIT CPU Inference Latency: {:.2f} ms / sample".format(
-        int8_jit_cpu_inference_latency * 1000))
 
 
 if __name__ == "__main__":
